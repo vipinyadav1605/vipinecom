7335600e2dcf4d212f06074813ac2152047ab082
vipinyadav1605 <vvvyadav223221@gmail.com>
2024-07-12 19:01:07 +0530

    first commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
         <div className="d-flex align-items-center justify-content-center pb-5">
           <div className="col-md-6">
             <p className="mb-3 mb-md-0">Made with ❤️ by {" "}
-              <a  href="https://github.com/praoo" className="text-decoration-underline text-dark fs-5" target="_blank" rel="noreferrer">Prabhat Kumar Singh</a>
+              <a  href="https://github.com/praoo" className="text-decoration-underline text-dark fs-5" target="_blank" rel="noreferrer">Vipin Yadav</a>
             </p>
             <a className="text-dark fs-4" href="https://github.com/praoo" target="_blank" rel="noreferrer">
               <i className="fa fa-github"></i>
